ff3447bbcaf71120ff12db604d04fb56367e2409
wajee-ul-hassan <wajeex001@gmail.com>
2024-03-13 01:05:06 +0500

    nav bar added

--- my_website/events/views.py
+++ my_website/events/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+import calendar
+from  calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html',{})
+def home(request, year=None, month=None):
+    if year is None:
+        year = datetime.now().year
+    if month is None:
+        month = datetime.now().strftime('%B')
+    month = month.title()
+    #convert month name to number
+    month_num=list(calendar.month_name).index(month)
+    month_num=int(month_num)
+    #creating a calender for you guyz don't you worry
+    cal=HTMLCalendar().formatmonth(year,month_num)
+
+    return render(request,'events/base.html',{
+        'month':month,
+        'year':year,
+        'month_num':month_num,
+        'cal':cal,
+    })
