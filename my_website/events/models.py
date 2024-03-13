a5e21f4ae3733d080fdadced01eb282443e3d23b
wajee-ul-hassan <wajeex001@gmail.com>
2024-03-13 15:06:38 +0500

    home

--- my_website/events/models.py
+++ my_website/events/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Venue(models.Model):
+    name=models.CharField(max_length=120)
+    address=models.CharField(max_length=120)
+    zip_code=models.CharField(max_length=120)
+    phone=models.CharField(max_length=12)
+    web=models.URLField()
+    email_address=models.EmailField()
+
+    
+    def __str__(self):
+        return self.name
+
+    
+class Member(models.Model):
+    first_name=models.CharField(max_length=120)
+    last_name=models.CharField(max_length=120)
+    email_address=models.EmailField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+       
+class Event(models.Model):
+    name=models.CharField(max_length=120)
+    event_date=models.DateTimeField()
+    venue=models.ForeignKey(Venue,on_delete=models.CASCADE,blank=True,null=True)
+    manager=models.CharField(max_length=120)
+    description=models.TextField()
+    members=models.ManyToManyField(Member,blank=True)
+ 
+    def __str__(self):
+        return self.name
+ 
--- my_website/events/views.py
+++ my_website/events/views.py
@@ -2,23 +2,13 @@ from django.shortcuts import render
 import calendar
 from  calendar import HTMLCalendar
 from datetime import datetime
+from .models import *
 
 # Create your views here.
-def home(request, year=None, month=None):
-    if year is None:
-        year = datetime.now().year
-    if month is None:
-        month = datetime.now().strftime('%B')
-    month = month.title()
-    #convert month name to number
-    month_num=list(calendar.month_name).index(month)
-    month_num=int(month_num)
-    #creating a calender for you guyz don't you worry
-    cal=HTMLCalendar().formatmonth(year,month_num)
-
+def home(request):
+    name="view the events here ..."
+    event_list=Event.objects.all()
     return render(request,'events/base.html',{
-        'month':month,
-        'year':year,
-        'month_num':month_num,
-        'cal':cal,
+        'name':name,
+        'event_list':event_list
     })
